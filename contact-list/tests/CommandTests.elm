78357e2388ae9855c0a5e9b41a849f6cf5ad55bc
Sébastien Besnier <sebastien.fabrice.besnier@gmail.com>
2021-10-28 07:44:56 +0200

    add some tests for command

--- contact-list/tests/CommandTests.elm
+++ contact-list/tests/CommandTests.elm
@@ -10,15 +10,30 @@ suite =
     describe "test commands"
         [ test "parse addPerson command" <|
             \_ ->
+                "add{name=Linus,age=42}"
+                    |> Command.parse
+                    |> Expect.equal (Ok (Command.addPerson "Linus" 42))
+        , test "parse addPerson with hyphen command" <|
+            \_ ->
+                "add{name=Jean-Michel,age=42}"
+                    |> Command.parse
+                    |> Expect.equal (Ok (Command.addPerson "Jean-Michel" 42))
+        , test "parse addPerson command with spaces in name" <|
+            \_ ->
                 "add{name=Linus Torvald,age=42}"
                     |> Command.parse
                     |> Expect.equal (Ok (Command.addPerson "Linus Torvald" 42))
-        , test "parse addPerson command with spaces" <|
+        , test "parse addPerson command with spaces everywhere" <|
             \_ ->
                 "add { name = Linus Torvald , age = 42 }"
                     |> Command.parse
                     |> Expect.equal (Ok (Command.addPerson "Linus Torvald" 42))
         , test "parse removePerson command" <|
+            \_ ->
+                "del Linus"
+                    |> Command.parse
+                    |> Expect.equal (Ok (Command.removePerson "Linus"))
+        , test "parse removePerson with space in name command" <|
             \_ ->
                 "del  Linus Torvald "
                     |> Command.parse
